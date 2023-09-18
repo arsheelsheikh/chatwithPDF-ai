43ce5af20424fb0489b22b9c7efbe85e8af75732
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-18 11:38:48 +0530

    setup clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    </div>
   )
 }

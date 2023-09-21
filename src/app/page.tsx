4a0f52a795fa670396f2d15db506c7fda25a8efd
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-21 19:50:41 +0530

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react"
 
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
 
-export default function Home() {
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && (
+              <Button>Go to Chats</Button>
+            )}
+          </div>
+
+            <p className="max-w-xl ml-1 text-lg text-slate-600">
+            Join millions of students, researchers and professinals to instantly
+            anwer questions and understand research with AI
+            </p>
+
+            <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }

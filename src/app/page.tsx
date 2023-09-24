0cfede5c0c8d1a682b657757980a3422e5e48e38
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-25 04:49:05 +0530

    FileUpload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import { LogIn } from "lucide-react"
+import FileUpload from "@/components/FileUpload";
 
 export default async function Home() {
   const { userId } = await auth();
@@ -28,7 +29,7 @@ export default async function Home() {
 
             <div className="w-full mt-4">
             {isAuth ? (
-              <h1>fileupload</h1>
+              <FileUpload />
             ) : (
               <Link href="/sign-in">
                 <Button>
